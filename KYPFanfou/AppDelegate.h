f7c2ad25f6c127fc783bff2cfaec47a67ce8ba31
Jaykong <kongyunpeng2011@sina.com>
2016-07-25 15:04:21 +0800

    First

---
+++ KYPFanfou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KYPFanfou
+//
+//  Created by trainer on 7/25/16.
+//  Copyright © 2016 trainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KYPFanfou/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  KYPFanfou
+//
+//  Created by trainer on 7/25/16.
+//  Copyright © 2016 trainer. All rights reserved.
+//
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+}
+
+@end
